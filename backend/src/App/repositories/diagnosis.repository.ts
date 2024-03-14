53fba0e84ff61390186f4e98be4ce104f1892353
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-03-14 20:15:44 -0300

    fixed diagnosis repository, addicted updated function

--- backend/src/App/repositories/diagnosis.repository.ts
+++ backend/src/App/repositories/diagnosis.repository.ts
@@ -2,6 +2,7 @@ import { DiagnosisEntity } from '../entites/diagnosi.entity';
 
 export abstract class DiagnosisRepository {
   abstract getAll(): Promise<DiagnosisEntity[]>;
+  abstract update(id: string, values): Promise<void>;
   abstract findById(id: string): Promise<DiagnosisEntity>;
   abstract insert(values): Promise<void>;
   abstract delete(id: string): Promise<void>;
